8097aa922fa047ee33d4e91fefb7690ed2f0b1d2
Aidan Underwood <61338453+aidan-underwood@users.noreply.github.com>
2025-01-24 16:45:33 +0000

    1/24

---
+++ intro1.py
@@ -0,0 +1,40 @@
+# File: intro1.py
+#
+# Purpose: To get introduced to Spyder (our IDE - Integrated
+#          Development Environment) and to our programming
+#          language Python.  Elementary features will 
+#          include how to print to the console and see
+#          usage of variables.
+# 
+# Programmer: Aidan Underwood
+
+
+# going to print three lines
+print("Hello world!")
+print("Hope you will enjoy programming!")
+print("Programming can be fun!")
+print()
+# going to use a variable now
+# and print its contents
+strHello = "Hello world again"
+print(strHello)
+print()
+# now for some numeric variables and printing
+# out their contents
+num1 = 5
+num2 = 9.4
+print(str(num1) + " " + str(num2))
+# print out the data types of these variables
+print(str(type(num1)) + " " + str(type(num2)))
+# change the data type of one of our variables
+num3 = int(9.4)
+print(num3)
+# print message to end program
+print("Goodbye")
+
+
+
+
+
+
+
