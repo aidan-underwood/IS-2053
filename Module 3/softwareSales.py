55fe2ca4e09b58dc9d54603082cd3a0bb4f3f6ed
Aidan Underwood <61338453+aidan-underwood@users.noreply.github.com>
2025-02-10 16:47:39 +0000

    finish module 3 and start module 4

--- Module 3/softwareSales.py
+++ Module 3/softwareSales.py
@@ -11,31 +11,53 @@
 
 
 # declare constant to be used in program
-
+RETAIL_PRICE = 99.00
 
 # prompt the user to enter how many packages they
 # have purchased, then read it in
-
+quantity = int(input("Enter the number of packages purchased: "))
 
 # calculate the discount rate using
 # logical operators
 
+#if (quantity >= 10 and quantity <= 19):
+#    discountRate = 0.10
+#elif (quantity >= 20 and quantity <= 49):
+#    discountRate = 0.20
+#elif (quantity >= 50 and quantity <= 99):
+#    discountRate = 0.30
+#elif (quantity >= 100):
+#    discountRate = 0.40
+#else:
+#    discountRate = 0.0
 
 # calculate again the discount rate but without
 # using logical operators
-
+if quantity < 9:
+    discountRate = 0.0
+elif quantity < 19:   
+    discountRate = 0.10
+elif quantity < 49:   
+    discountRate = 0.20
+elif quantity < 99:  
+    discountRate = 0.30
+else:                
+    discountRate = 0.40
 
 # calculate the full price
-
+fullPrice = quantity * RETAIL_PRICE
 
 # calculate the discount amount
-
+discountAmount = fullPrice * discountRate
 
 # calculate the total amount (i.e., take
 # off the discount from the full price)
-
+finalAmount = fullPrice - discountAmount
 
 # print out the results
-
+print("\nFull amount (before discount): $", format(fullPrice, ',.2f'))
+print("Discount amount: $", format(discountAmount, ',.2f'))
+print("Final amount: $", format(finalAmount, ',.2f'))
 
 # print message to end program
+print("\nThis program was writtn by Aidan Underwood" + "\nEnd of program.")
---
+++ Module 4/sumPositive.py
@@ -0,0 +1,52 @@
+# File: sumPositive.py
+# 
+# Purpose: Adds up all positive floating-point
+#          numbers entered by the user.  A while
+#          loop is used to accomplished this.  Adding
+#          stops once the user has entered a negative
+#          number.  Calculate the average of these
+#          numbers.  Final value is then printed out.
+#
+# Programmer: Aidan Underwood
+
+
+# set up accumulator variables to hold the summation
+# of positive numbers and total number of numbers
+# entered
+total = 0.0
+count = 0
+
+# prompt the user to enter a floating-point
+# number, then read it in
+number = float(input("Enter a floating-point number (negative to quit): "))
+
+# using a while loop, add up all positive
+# numbers entered by the user until a negative 
+# number is entered
+while (number > 0):
+    
+    # increase value of how many positive
+    # numbers the user has entered
+    count += 1
+    
+    # add value of 'number' to 'total' only 
+    # if it is positive
+    if number > 0:
+        total = total + number
+        
+    # prompt the user to enter a floating-point
+    # number, then read it in
+    number = float(input("Enter a floating-point number (negative to quit): "))
+# end while loop
+
+# calculate average
+if count != 0:
+    average = total / count
+else:
+    average = 0.0
+# display the results
+print("\nFinal value of positive numbers: ", format(total, ',.2f'))
+print("Average was: ", format(average, ',.2f'))
+print("There were",count,"numbers entered.")
+# print message to end program
+print("\nThis program was writtn by Aidan Underwood" + "\nEnd of program.")
